a198541ac956c27ebdb3510f8dfe9002d6451bbe
BomberAbhi17 <bomberabhi17@gmail.com>
2022-10-27 18:34:47 +0530

    adding Dynamic programming Sol CSES

---
+++ Searching And Sorting/SubarraySums1.cpp
@@ -0,0 +1,76 @@
+
+#pragma GCC optimize("Ofast")
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimization ("unroll-loops")
+ 
+
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+ 
+               
+ 
+
+
+
+#define mod 1000000007
+#define ll long long
+ 
+#define pb push_back 
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define gcd(a,b)    __gcd((a),(b))
+#define lcm(a,b)    ((a)*(b)) / gcd((a),(b))
+#define all(a)              a.begin(),a.end()
+#define rsrt(v) sort(v.begin(), v.end(), greater<int>())
+#define rsrtl(v) sort(v.begin(), v.end(), greater<ll>())
+#define fi(i, a, b) for (int i = a; i < b; i++)
+#define fll(i, a, b) for (ll i = a; i < b; i++)
+#define rfi(i, a, b) for (int i = int(a); i >= int(b); i--)
+#define mxe(v) *max_element(v.begin(), v.end())
+#define srt(v) sort(v.begin(), v.end())
+#define g(v, i, j) get<i>(v[j])
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define F first
+#define S second
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define mp make_pair
+#define sz(x) (int)x.size()
+#define sp " "
+#define nl '\n'
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    ll n,x,i,j,k;
+    cin>>n>>x;
+    ll a[n];
+    map<ll,ll> psumcnt;
+    psumcnt[0]=1;
+    ll psum=0;
+    ll ans=0;
+    fll(i,0,n){
+        cin>>a[i];
+        psum+=a[i];
+        ans+=psumcnt[psum-x];
+        psumcnt[psum]++;
+        
+    }
+    cout<<ans<<"\n";
+    
+  
+
+
+
+
+    return 0;
+
+}
